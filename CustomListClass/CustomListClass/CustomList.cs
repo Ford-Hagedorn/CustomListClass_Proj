077b055377ce80d875d988863e4e51d4ba3af5c0
Fordley Hagedorn <fordleyh2900@gmail.com>
2021-02-22 12:13:06 -0600

    Init

---
+++ CustomListClass/CustomListClass/CustomList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListClass
+{
+    public class CustomList<T>
+    {
+
+        public void Add()
+        {
+
+            for (int i = 0; i < numberToAdd; i++)
+            {
+
+            }
+        }
+
+        public void Remove()
+        {
+
+        }
+    }
+}
---
+++ CustomListClass/CustomListClass/Subtraction.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListClass
+{
+    [TestClass]
+    public class Subtraction
+    {
+        [TestMethod]
+
+        public void Subtract_OneFromCount()
+        {
+
+        }
+    }
+}
