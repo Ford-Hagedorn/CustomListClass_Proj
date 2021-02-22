e8cf50f90bc1cd88ecd9d3acb703077bed147b35
Fordley Hagedorn <fordleyh2900@gmail.com>
2021-02-22 16:14:48 -0600

    Removed unnecessary classes, added testing classes, added test methods for checking the initial list and adding things. Need to work on the count and capacity

---
+++ CustomListClass/CustomListUnitTest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListUnitTest1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListUnitTest1")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("518b1fee-a373-4152-8912-74cabf041a8d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CustomListClass/CustomListUnitTest1/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CustomListClass;
+
+namespace CustomListUnitTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckListCapacity()
+        {
+            //Arrange
+            CustomList<int> firstCheck = new CustomList<int>();
+            int expected = 4;
+            int actual;
+            //Act
+            actual = firstCheck.Capacity;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+
+        public void Add_OneToCount()
+        {
+            //Arrange
+            CustomList<int> addOne = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            //Act
+            actual = addOne.Count;
+            addOne.Count++;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+
+        public void Add_Multiple()
+        {
+            //Arrange
+            CustomList<int> addExtra = new CustomList<int>();
+            int expected = 5;
+            int actual;
+            //Act
+            actual = addExtra.Count;
+            if (addExtra.Count < 5)
+            {
+                addExtra.Count++;
+            }
+            //Assert
+            Assert.AreEqual(expected, actual);
+            
+
+        }
+        [TestMethod]
+
+        public void Add_Invalid()
+        {
+            //Arrange
+            CustomList<int> oops = new CustomList<int>();
+            int expected = 6;
+            int actual;
+            //Act
+            actual = oops.Count;
+            if (oops.Count < 5)
+            {
+                oops.Count++;
+            }
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
