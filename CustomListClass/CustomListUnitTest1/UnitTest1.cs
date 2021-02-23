6c4aec5e6b4427a5aff60fa8a0922c7a29373f26
Fordley Hagedorn <fordleyh2900@gmail.com>
2021-02-23 11:16:20 -0600

    Made test methods for add and remove, 5 each

--- CustomListClass/CustomListUnitTest1/UnitTest1.cs
+++ CustomListClass/CustomListUnitTest1/UnitTest1.cs
@@ -64,13 +64,106 @@ namespace CustomListUnitTest1
             int actual;
             //Act
             actual = oops.count;
-            if (oops.count < 5)
-            {
-                oops.count++;
-            }
-            Assert.AreEqual(expected, actual);
+            oops.count++;
+            //Assert
+            Assert.AreNotEqual(expected, actual);
 
         }
         [TestMethod]
+
+        public void Add_Nothing()
+        {
+            //Arrange
+            CustomList<int> nothingNew = new CustomList<int>();
+            int expected = 0;
+            int actual;
+            //Act
+            actual = nothingNew.count;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+
+        public void Add_DoubleCount()
+        {
+            //Arrange
+            CustomList<int> timesTwo = new CustomList<int>();
+            int expected = 8;
+            int actual;
+            //Act
+            actual = timesTwo.count * 2;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        //Removing Method Tests
+        [TestMethod]
+
+        public void Remove_One()
+        {
+            //Arrange
+            CustomList<int> minusOne = new CustomList<int>();
+            int expected = -1;
+            int actual;
+            //Act
+            actual = minusOne.count;
+            minusOne.count--;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+
+        public void Remove_Multiple()
+        {
+            //Arrange
+            CustomList<int> minusExtra = new CustomList<int>();
+            int expected = -5;
+            int actual;
+            //Act
+            actual = minusExtra.count;
+            minusExtra.count--;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+
+        public void Remove_Invalid()
+        {
+            //Arrange
+            CustomList<int> oopsies = new CustomList<int>();
+            int expected = -6;
+            int actual;
+            //Act
+            actual = oopsies.count;
+            oopsies.count--;
+            //Assert
+            Assert.AreNotEqual(expected, actual);
+        }
+        [TestMethod]
+
+        public void Remove_Nothing()
+        {
+            //Arrange
+            CustomList<int> nothingLost = new CustomList<int>();
+            int expected = 0;
+            int actual;
+            //Act
+            actual = nothingLost.count;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+
+        public void Remove_Half()
+        {
+            //Arrange
+            CustomList<double> divideByTwo = new CustomList<double>();
+            double expected = 0.5;
+            double actual;
+            //Act
+            actual = divideByTwo.count / 2;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
