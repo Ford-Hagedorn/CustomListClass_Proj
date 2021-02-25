cb0b63916275f395c75dd8c4ab9c46691b297418
Fordley Hagedorn <fordleyh2900@gmail.com>
2021-02-25 13:06:53 -0600

    Trying my best to make the Remove method work. Saving so I dontlose progress.

--- CustomListClass/CustomListClass/CustomList.cs
+++ CustomListClass/CustomListClass/CustomList.cs
@@ -8,7 +8,7 @@ namespace CustomListClass
 {
     public class CustomList<T>
     {
-        public T[] _numbers;
+        public T[] numbers;
         private int count;
         public int Count { get => count; }
 
@@ -16,14 +16,14 @@ namespace CustomListClass
         private int capacity;
 
         public int Capacity { get => capacity; }
-        public T this[int index] { get => _numbers[index]; set => _numbers[index] = value; }
+        public T this[int index] { get => numbers[index]; set => numbers[index] = value; }
 
 
         public CustomList()
         {
             count = 0;
             capacity = 4;
-            _numbers = new T[capacity];
+            numbers = new T[capacity];
         }
 
         public void Add(T numberToAdd)
@@ -32,23 +32,58 @@ namespace CustomListClass
             {
                 capacity = capacity * 2;
                 T[] biggerNumber = new T[capacity];
-                _numbers[count] = numberToAdd;
+                numbers[count] = numberToAdd;
                 for (int i = 0; i < count; i++)
                 {
-                    biggerNumber[i] = _numbers[i];
+                    biggerNumber[i] = numbers[i];
                 }
-                _numbers = biggerNumber;
+                numbers = biggerNumber;
                 
             }
-            _numbers[count] = numberToAdd;
+            numbers[count] = numberToAdd;
             count++;
 
         }
 
         public void Remove(T numberToSubtract)
         {
-            _numbers[count] = numberToSubtract;
-            count--;
+            //make array with values : numberSpread
+            //int value
+            //bool removeValue(?)
+            //remove first iteration of value, then stop
+            //first iteration removed = true
+            //keep going until the value is removed
+            //T[] numberSpread = new T[count];
+            //T[] newNumberSpread = new T[count - 1];
+            //int i = 0;
+            //int j = 0;
+            //numbers[count] = numberToSubtract;
+            //while (i < numberSpread.Length)
+            //{
+            //    if (i != numberToSubtract)
+            //    {
+            //        newNumberSpread[j] = numberSpread[i];
+            //        j++;
+            //    }
+            //    i++;
+            //}
+            //count--;
+            T[] numberSpread = new T[count];
+            T[] newNumberSpread = new T[count - 1];
+            bool removeItem = true;
+            int i = 0;
+            int j = 0;
+            while (i < numberSpread.Length)
+            {
+                int numberRemove;
+
+
+                if (i != numberRemove)
+                {
+                    
+                }
+            }
+
         }
     }
 }
